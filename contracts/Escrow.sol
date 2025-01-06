196624b165754ad7abc84e44d26a9b9016a7413d
Akinmolayan John <112864232+Johntech-dev@users.noreply.github.com>
2025-01-06 14:03:26 +0100

    john

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -10,5 +10,26 @@ interface IERC721 {
 }
 
 contract Escrow {
+address public nftAddress;
+address public seller;
+address public inspector;
+address public lender;
+
+  mapping(uint256 => bool) public isListed;
+
+constructor(address _nftAdress, address payable_seller, address _inspector, address  _lender) {
+    nftAddress = _nftAdress;
+    seller = payable_seller;
+    inspector = _inspector;
+    lender = _lender;
+}
+
+function list(uint256 _nftID) public {
+   
+  
+  // Transfer NFT from seller t this contract
+  IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+  isListed[_nftID];
+}
 
 }
